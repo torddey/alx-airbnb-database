c9b76e14e4c08012a52d5afc72bba139ed9a03b4
torddey <ea.torddey@gmail.com>
2025-05-16 13:46:35 +0000

    added windows functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -15,3 +15,25 @@ ORDER BY
 
 
 --PROPERTY RANKING (WINDOW FUNCTIONS)--
+WITH PropertyBookingCounts AS (
+    SELECT
+        p.property_id,
+        p.name,
+        COUNT(b.booking_id) AS total_bookings
+    FROM 
+        PROPERTY p 
+    LEFT JOIN
+        BOOKING b ON p.property_id = b.property_id
+    GROUP BY
+        p.property_id, p.name
+)
+SELECT
+    property_id,
+    name,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_num_rank,
+    RANK() OVER (ORDER BY total_bookings DESC) AS rank_num
+FROM 
+    PropertyBookingCounts
+ORDER BY 
+    total_bookings DESC;
